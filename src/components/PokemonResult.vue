bd2a173d05603c503127e5cb98604eb1e930a71a
Anthony <blacknebula236@gmail.com>
2025-04-04 23:05:35 +1030

    individual pokemon result

---
+++ src/components/PokemonResult.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="card bg-base-300 rounded-box grid grow place-items-center">
+        <h1>{{ capitalized(pokemon?.data.name) }}</h1>
+        <img :src=pokemon?.data.sprites.front_default />
+    </div>
+</template>
+
+<script setup lang="ts">
+import type { PokemonResource } from '../stores/search';
+
+const props = defineProps<{
+    pokemon?: PokemonResource | null
+}>()
+
+const capitalized = (word: string) => {
+    return word.charAt(0).toUpperCase() + word.slice(1)
+}
+
+</script>
