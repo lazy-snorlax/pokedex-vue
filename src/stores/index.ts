361415c16d072f744c66174ddd5a17523aedaa99
Anthony <blacknebula236@gmail.com>
2025-04-05 12:08:34 +1030

    remove uneeded file

--- src/stores/index.ts
+++
@@ -1,9 +0,0 @@
-import { createPinia } from "pinia";
-// import { http } from "../utils/http";
-
-export const pinia = createPinia()
-
-//  Set HTTP client as a custom property for all stores
-// pinia.use(() => ({ http }))
-
-// TODO: enable custom resorce plugins
