e2e6ebbc65146cb506b2162325b29e27db26ac2b
Anthony <blacknebula236@gmail.com>
2025-04-05 12:16:46 +1030

    remove watch + use advList in store

--- src/components/PokemonResults.vue
+++ src/components/PokemonResults.vue
@@ -1,22 +1,32 @@
 <template>
-    <div class="grid grid-cols-1 gap-8 xl:grid-cols-6 lg:grid-cols-4 md:grid-cols-3">
-        <div v-for="pokemon in list">
+    <div class="grid grid-cols-1 gap-8 xl:grid-cols-6 lg:grid-cols-4 md:grid-cols-3 mb-5">
+        <div v-for="pokemon in advList">
             <PokemonResult :pokemon />
         </div>
     </div>
 </template>
 
 <script setup lang="ts">
-import { watch } from 'vue';
+import { onMounted } from 'vue';
 import type { PokemonListResource } from '../stores/search';
 import PokemonResult from './PokemonResult.vue';
+
+import { useSearchStore } from '../stores/search';
+import { storeToRefs } from 'pinia';
+
+const { advList } = storeToRefs(useSearchStore())
+const { getPokeData } = useSearchStore()
 
 const props = defineProps<{
     list?: Array<PokemonListResource> | null
 }>()
 
-watch(props.list, () => {
-    console.log(">>> List: ", props.list)
+onMounted(() => {
+    getPokemon(props.list)
 })
+
+const getPokemon = async (list) => {
+    await getPokeData(list)
+}
 
 </script>
