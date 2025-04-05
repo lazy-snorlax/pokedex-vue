386b463779324f6a7c42f4e93047b734f3d0c3cc
Anthony <blacknebula236@gmail.com>
2025-04-05 12:09:49 +1030

    clean mega & gmax names + use offical artwork

--- src/components/PokemonResult.vue
+++ src/components/PokemonResult.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="card bg-base-300 rounded-box grid grow place-items-center">
         <h1>{{ capitalized(pokemon?.data.name) }}</h1>
-        <img :src=pokemon?.data.sprites.front_default />
+        <img :src=" pokemon.data.sprites.other['official-artwork'].front_default" />
     </div>
 </template>
 
@@ -12,8 +12,17 @@ const props = defineProps<{
     pokemon?: PokemonResource | null
 }>()
 
-const capitalized = (word: string) => {
-    return word.charAt(0).toUpperCase() + word.slice(1)
+const capitalized = (name: string) => {
+    if (name.includes("mega") || name.includes("gmax")) {
+        let words = name.split("-")
+        let newWord = ""
+        words.forEach(word => {
+            newWord += word.charAt(0).toUpperCase() + word.slice(1) + " "
+        });
+        return newWord
+    } else {
+        return name.charAt(0).toUpperCase() + name.slice(1)
+    }
 }
 
 </script>
