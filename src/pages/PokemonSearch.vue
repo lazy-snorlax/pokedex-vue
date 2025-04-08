273d2fee8804b54ad9803853645c33079ed24404
Anthony <blacknebula236@gmail.com>
2025-04-08 19:52:34 +0930

    flavour text fix

--- src/pages/PokemonSearch.vue
+++ src/pages/PokemonSearch.vue
@@ -4,7 +4,7 @@
         <div class="max-w-full">
           <h1 class="text-5xl font-bold">Pokedex Search</h1>
           <p class="py-6">
-            Type at least 3 characters to begin a search
+            Type at least 3 letters to begin a search
           </p>
           <div class="rounded-box grid h-20 place-items-center">
             <PokemonSearch @filtered-list="filterResults" />
