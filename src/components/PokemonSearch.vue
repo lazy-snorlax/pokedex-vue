bc4f71331da64699dd20bbc631beb7cc69a7a7aa
Anthony <blacknebula236@gmail.com>
2025-07-24 10:12:18 +0930

    refactor search to on click event

--- src/components/PokemonSearch.vue
+++ src/components/PokemonSearch.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="form-control">
         <div class="relative">
-            <input type="text" class="w-full pr-40 bg-gray-200 input input-lg text-black" placeholder="Search..." @change=handleChange />
-            <button type="button" class="absolute top-0 right-0 rounded-l-none w-36 btn btn-primary btn-lg">Search</button>
+            <input type="text" class="w-full pr-40 bg-gray-200 input input-lg text-black" placeholder="Search..." v-model="searchStr" />
+            <button type="button" class="absolute top-0 right-0 rounded-l-none w-36 btn btn-primary btn-lg" @click="search">Search</button>
         </div>
     </div>
 </template>
@@ -11,15 +11,15 @@
 
 import { storeToRefs } from 'pinia'
 import { useSearchStore, type PokemonListResource } from '../stores/search'
+import { ref } from 'vue'
 
 const { basicList } = storeToRefs(useSearchStore())
+const searchStr = ref("")
 
-const handleChange = (e: Event) => {
-    let searchStr = e.target?.value.toLowerCase()
-
-    if (searchStr.length >= 3) {
+const search = () => {
+    if (searchStr.value.length >= 3) {
         let filtered = basicList.value.filter((pokemon) => {
-            return pokemon.name.includes(searchStr)
+            return pokemon.name.includes(searchStr.value.toLowerCase())
         })
         emit('filteredList', filtered)
     }
