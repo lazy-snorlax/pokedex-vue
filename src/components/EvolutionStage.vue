3c1ceb479d5ae17a0479c5d2ac40d7605c0bea58
Anthony <blacknebula236@gmail.com>
2025-07-27 13:35:12 +0930

    create evolution component

---
+++ src/components/EvolutionStage.vue
@@ -0,0 +1,26 @@
+<template>
+  <li>
+    <div style="display: flex; align-items: center; gap: 1rem;">
+      <img :src="stage.sprite" :alt="stage.species" width="80" />
+      <div>
+        <strong>{{ stage.species }}</strong><br />
+        <em>{{ stage.evolutionInfo }}</em>
+      </div>
+    </div>
+
+    <ul v-if="stage.evolvesTo.length > 0" style="margin-left: 2rem;">
+      <EvolutionStage v-for="child in stage.evolvesTo" :key="child.species" :stage="child" />
+    </ul>
+  </li>
+</template>
+
+<script lang="ts" setup>
+defineProps<{
+  stage: {
+    species: string
+    sprite: string
+    evolutionInfo: string
+    evolvesTo: any[]
+  }
+}>()
+</script>
